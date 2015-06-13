a41581a31478ab1247bd7d14631955f71fa99506
OSMC-mt7601 <gregory954@gmail.com>
2015-06-13 19:58:25 -0300

    Upload to Github
    First Commit

---
+++ PNG2BIN.php
@@ -0,0 +1,18 @@
+<?php
+$im = imagecreatefrompng("IMG.png");
+$a=fopen("output.bin","w");
+for($y=0;$y<64;$y=$y+8){
+    for($x=0;$x<128;$x++){
+        $data="";
+        for($y2=$y;$y2<$y+8;$y2++){
+            if(imagecolorat($im,$x,$y2)==0){
+                $data.="0";
+            }else{
+                $data.="1";
+                }
+        }
+    fputs($a,pack('H*', str_pad(base_convert($data, 2, 16), 2, "0", STR_PAD_LEFT)));
+    }
+}
+fclose($a);
+?>
