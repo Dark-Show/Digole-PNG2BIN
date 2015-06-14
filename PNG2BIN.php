583b6084f6b74bf5b51258afe66bc6883cff42f2
Gregory Michalik <gregory954@gmail.com>
2015-06-14 20:36:01 -0300

    Add some small comments

--- PNG2BIN.php
+++ PNG2BIN.php
@@ -1,17 +1,17 @@
 <?php
 $im = imagecreatefrompng("IMG.png");
 $a=fopen("output.bin","w");
-for($y=0;$y<64;$y=$y+8){
-    for($x=0;$x<128;$x++){
+for($y=0;$y<64;$y=$y+8){                        //Y=64
+    for($x=0;$x<128;$x++){                      //X=128
         $data="";
-        for($y2=$y;$y2<$y+8;$y2++){
+        for($y2=$y;$y2<$y+8;$y2++){             //Fetch 8 points to make up a byte
             if(imagecolorat($im,$x,$y2)==0){
                 $data.="0";
             }else{
                 $data.="1";
             }
         }
-    fputs($a,pack('H*', str_pad(base_convert($data, 2, 16), 2, "0", STR_PAD_LEFT)));
+    fputs($a,pack('H*', str_pad(base_convert($data, 2, 16), 2, "0", STR_PAD_LEFT))); //Pack bits into a byte and write to file.
     }
 }
 fclose($a);
