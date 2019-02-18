9e4d3cd64e77a44545f8be8879e4ca8aa0f6aabc
Gregory Michalik <gregory954@gmail.com>
2019-02-18 17:55:54 -0400

    Update PNG2BIN.php
    Extended comments

--- PNG2BIN.php
+++ PNG2BIN.php
@@ -1,18 +1,33 @@
 <?php
+
+// 128x64 monotone image
+// Format: 
+// Headerless
+// Formated as 8 B/W pixels per byte
+// 8 horizontal blocks of 128x8 (128 bytes)
+
 $im = imagecreatefrompng("IMG.png");
 $a = fopen("output.bin", "w");
-for($y = 0; $y < 64; $y = $y+8){  //Y=64
-    for($x = 0; $x < 128; $x++){  //X=128
-        $data = "";
-        for($y2 = $y; $y2 < $y + 8; $y2++){  //Fetch 8 points to make up a byte
-            if(imagecolorat($im, $x, $y2) == 0){
-                $data .= "0";
+
+// Step through height
+for($y = 0; $y < 64; $y = $y+8){  // Y = 0 - 63 , 8 Step
+    // Step through width
+    for($x = 0; $x < 128; $x++){  // X = 0 - 127, Single step
+        $byte = "";
+        
+        //Fetch 8 points (bits) to make up a byte
+        for($bit = $y; $bit < $y + 8; $bit++){
+            // trun black and white only
+            if(imagecolorat($im, $x, $bit) == 0){
+                $byte .= "0";
             }else{
-                $data .= "1";
+                $byte .= "1";
             }
         }
-    $byte = pack('H*', str_pad(base_convert($data, 2, 16), 2, "0", STR_PAD_LEFT)); //Pack bits into a byte.
-    fputs($a, $byte);
+        
+        //Pack our bits into a byte.
+        $byte = pack('H*', str_pad(base_convert($byte, 2, 16), 2, "0", STR_PAD_LEFT));
+        fputs($a, $byte);
     }
 }
 fclose($a);
